92e534cbf795b69c681b850435123529aeba5672
123soniyo <shrutikristm2001@gmail.com>
2024-03-06 12:05:39 +0530

    add more funtionality

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const userRoutes = require('./routes/user')
+const bodyParser = require('body-parser')
+const sequelize = require('./util/database')
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+
 app.use(userRoutes)
-app.listen(2000)
+
+
+
+
+sequelize
+  .sync({
+    force:false
+  })
+  .then(result => {
+    // console.log(result);
+    app.listen(2000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
